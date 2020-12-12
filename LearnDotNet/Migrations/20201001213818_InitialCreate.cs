fe20c0cc3b407b0c34debda7a23a35bbd0be8b37
Sabin Chhetri <sabin.chhetri@itsutra.com>
2020-12-12 19:04:46 +0545

    initial

---
+++ LearnDotNet/Migrations/20201001213818_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LearnDotNet.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DCandidates",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(nullable: true),
+                    Mobile = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    BloodGroup = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DCandidates", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DCandidates");
+        }
+    }
+}
